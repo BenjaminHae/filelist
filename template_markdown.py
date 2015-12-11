80bbe060ac2bf33bb96f993a9d719a8a69ad4395
Benjamin Häublein <benjaminh@benjamin-VAIO.lan.benjaminh.de>
2015-12-11 11:14:07 +0100

    implemented markdown template

--- template_markdown.py
+++ template_markdown.py
@@ -1,9 +1,10 @@
-html = {
-'begin':lambda title: title,
+template_markdown = {
+'begin':lambda title: title+"\r\n"+"="*len(title)+"\r\n",
 'end':'',
-'headers':lambda name: name,
+'headers':lambda name: name+"\r\n"+"-"*len(name)+"\r\n",
+
 'filelist_begin':'',
 'filelist_end':'',
-'file':lambda name: name,
-'folder':lambda id, name, filelist:id+name+filelist,
+'file':lambda name: "  * "+name+"  \r\n",
+'folder':lambda id, name, filelist:"  * **"+name+"**  \r\n"+''.join(map(lambda s: " "*4+s, filelist.splitlines(True)))+"\r\n",
 }
