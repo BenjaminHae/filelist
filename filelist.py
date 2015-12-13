01e5500ad7109b38d75c27715ae655618a8d8581
Benjamin H盲ublein <BenjaminHae@users.noreply.github.com>
2015-12-13 11:26:18 +0100

    hide hidden files

--- filelist.py
+++ filelist.py
@@ -10,6 +10,7 @@ parser.add_argument('--top', '-t', action='store_true', help='use the top folder
 parser.add_argument('--ignore-subdirs', '-is', action='store_true', help='ignore all subfolders')
 parser.add_argument('--title', type=str, action='store', help='title of the webpage')
 parser.add_argument('--type', type=str, action='store', default='html', choices=['html','markdown'],  help='type of output')
+parser.add_argument('--all', '-a', action='store_true', help='show hidden files(UNIX only)')
 parser.add_argument('path', nargs='*', type=str, action='store', help='folders to print')
 args = parser.parse_args()
 
@@ -18,27 +19,30 @@ template=eval('template_'+args.type)
 folderId = 0
 def AddFile(file):
   return template['file'](file)
-def AddDir(dir, id, top = False):
+def AddDir(dir, id, top = False, showHidden = False):
   if top:
       res = lambda filelist: template['headers'](basename(dir))+filelist
   else:
       res = lambda filelist: template['folder'](str(id),basename(dir), filelist)
-  return res(AddFilelist(dir))
+  return res(AddFilelist(dir, showHidden = showHidden))
   
-def AddFilelist(dir, top = False):
+def AddFilelist(dir, top = False, showHidden = False):
   global folderId
   if top:
     result = ""
   else:
     result = template['filelist_begin']
   oldFolderId = folderId
-  for d in sorted(listdir(dir)):
+  content = sorted(listdir(dir))
+  if not showHidden:
+    content = filter( lambda f: not f.startswith('.'), content)
+  for d in content:
     if not isfile(join(dir,d)):
       folderId += 1
-      result += AddDir(join(dir,d),folderId, top)
+      result += AddDir(join(dir,d),folderId, top, showHidden)
   if top and oldFolderId == folderId:
     result += template['headers'](basename(dir))
-  for f in sorted(listdir(dir)):
+  for f in content:
     if isfile(join(dir,f)):
       result += AddFile(f)
   if top:
@@ -63,6 +67,6 @@ res = template['begin'](title)
 for folder in path:
   if printHead and not args.top:
     res += template['headers'](basename(folder))
-  res += AddFilelist(folder,args.top)
+  res += AddFilelist(folder, args.top, args.all)
 res += template['end']
 print res
